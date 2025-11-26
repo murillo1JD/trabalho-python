ae99d772ba630157d2ce9601b6184f65f82b94a9
dead¿ poggers <murillo.aparecido@aluno.educa.go.gov.br>
2025-11-26 10:55:56 -0300

    correçao FUNDAMENTAL usando copilot

--- quantidade de dano.py
+++ quantidade de dano.py
@@ -1,22 +1,26 @@
 dano_base = 100
 nivel_player = 60
 dano_bonus = 20
-is_exposto = True
+is_exposto = False
 dano_exposto = 1.5
 
-# dano baseado na exposição
-if is_exposto == True:
-    dano_total = (dano_base + dano_bonus) * dano_exposto
-elif not is_exposto:
-    dano_total = dano_base + dano_bonus
+# Calcula o dano
+if nivel_player >= 50:
+    if is_exposto:
+        dano_total = (dano_base + dano_bonus) * dano_exposto
+    else:
+        dano_total = dano_base + dano_bonus
 else:
-    dano_total = dano_base
+    if is_exposto:
+        dano_total = dano_base * dano_exposto
+    else:
+        dano_total = dano_base
 
 # Mensagem baseada no nível
 if nivel_player >= 50:
     if is_exposto:
-        print(f'Você deu {dano_total} de dano. o inimigo estava exposto, recebendo {dano_exposto}x.')
+        print(f'Você deu {dano_total} de dano. O inimigo estava exposto, recebendo {dano_exposto}x.')
     else:
         print(f'Você deu {dano_total} de dano, o inimigo não estava exposto.')
 else:
-    print(f'Você deu {dano_base} de dano. Evolua até nível 50 para receber bônus de {dano_bonus}.')
+    print(f'Você deu {dano_total} de dano. Evolua até nível 50 para receber bônus de {dano_bonus}.')
