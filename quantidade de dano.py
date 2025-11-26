381d87612fdb65e7d383ce31348bd80b29f10ef0
dead¿ poggers <murillo.aparecido@aluno.educa.go.gov.br>
2025-11-26 09:47:05 -0300

    adicionando mais variaçoes de dano

--- quantidade de dano.py
+++ quantidade de dano.py
@@ -1,10 +1,22 @@
 dano_base = 100
 nivel_player = 60
 dano_bonus = 20
+is_exposto = True
+dano_exposto = 1.5
 
-dano_total = dano_base + dano_bonus
-
-if nivel_player >= 50:
-   print(f'voce atualmente dá {dano_total} de dano, pois recebeu {dano_bonus} de bonus dmg ')
+# dano baseado na exposição
+if is_exposto:
+    dano_total = (dano_base + dano_bonus) * dano_exposto
+elif not is_exposto:
+    dano_total = dano_base + dano_bonus
 else:
-    print(f'voce dá atualmente {dano_base} de dano, evolua ate o 50 ou mais para receber um bonus de {dano_bonus}.')
+    dano_total = dano_base
+
+# Mensagem baseada no nível
+if nivel_player >= 50:
+    if is_exposto:
+        print(f'Você deu {dano_total} de dano, o inimigo estava exposto. recebendo {dano_exposto}x.')
+    else: # se nao estiver exposto
+        print(f'Você deu {dano_total} de dano, o inimigo não estava exposto.')
+else:
+    print(f'Você deu {dano_base} de dano. Evolua até nível 50 para receber bônus de {dano_bonus}.')
