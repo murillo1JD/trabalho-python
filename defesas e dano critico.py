bab2b9868d5b9eea025bfc2fae830eec5abc47d4
dead¿ poggers <murillo.aparecido@aluno.educa.go.gov.br>
2025-11-27 11:03:15 -0300

    MUITAS correçoes

--- defesas e dano critico.py
+++ defesas e dano critico.py
@@ -1,29 +1,21 @@
 import random
 dano_base = 150
-houve_critico = True
+houve_critico = random.random() < 0.3
 bonus_critico = 50
 defesa = 0.85
 defesa_santa = 0
 defesa_reflexiva = 0.45
-tipo_defesa_inimigo = 'Santa'
-dano_temporario = dano_base
-
-dano_base = 150
-houve_critico = True
-bonus_critico = 50
-
 defesas = ["Fortificada", "Santa", "Nenhuma", "Reflexiva"]
-
 tipo_defesa_inimigo = random.choice(defesas)
+dano_temporario = dano_base
 
 print(f"O inimigo escolheu a defesa: {tipo_defesa_inimigo}")
 
+print(f'houve_critico? {houve_critico}')
 if houve_critico:
     dano_base += bonus_critico
 else:
     dano_base
-    
-dano_temporario = dano_base
 
 if tipo_defesa_inimigo == 'Fortificada':
    dano_temporario = dano_temporario * defesa
@@ -46,5 +38,5 @@ elif tipo_defesa_inimigo == 'Nenhuma':
     print(f'voce causou {dano_temporario} de dano.')
     print(f'o inimigo estava usando defesa {tipo_defesa_inimigo}')
 elif tipo_defesa_inimigo == 'Reflexiva':
-    print(f'voce causou {dano_base} de dano.')
+    print(f'voce causou {dano_temporario} de dano.')
     print(f'O inimigo usou {tipo_defesa_inimigo} e você recebeu {dano_temporario} de volta.')
