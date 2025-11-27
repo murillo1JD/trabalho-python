d27469f801caff352ccc9061753ea8d539011805
dead¿ poggers <murillo.aparecido@aluno.educa.go.gov.br>
2025-11-27 10:30:04 -0300

    adiçao de escolha de defesa aleatória e algumas mudanças

--- defesas e dano critico.py
+++ defesas e dano critico.py
@@ -1,11 +1,22 @@
+import random
 dano_base = 150
 houve_critico = True
 bonus_critico = 50
 defesa = 0.85
 defesa_santa = 0
 defesa_reflexiva = 0.45
-tipo_defesa_inimigo = 'Reflexiva'
+tipo_defesa_inimigo = 'Santa'
 dano_temporario = dano_base
+
+dano_base = 150
+houve_critico = True
+bonus_critico = 50
+
+defesas = ["Fortificada", "Santa", "Nenhuma", "Reflexiva"]
+
+tipo_defesa_inimigo = random.choice(defesas)
+
+print(f"O inimigo escolheu a defesa: {tipo_defesa_inimigo}")
 
 if houve_critico:
     dano_base += bonus_critico
@@ -25,17 +36,15 @@ else:
 
 if dano_temporario == defesa_santa:
     defesa_santa = 'voce nao causou nenhum dano.'
-
+    
 if tipo_defesa_inimigo == 'Fortificada':
     print(f'voce causou {dano_temporario}')
-    print(f'o inimigo estava usando defesa {tipo_defesa_inimigo}, diminuindo seu dano em 15%')
-    
+    print(f'o inimigo estava usando defesa {tipo_defesa_inimigo}, diminuindo seu dano em 15%') 
 elif tipo_defesa_inimigo == 'Santa':
     print(f'voce causou {defesa_santa} o inimigo usou a defesa {tipo_defesa_inimigo}')
 elif tipo_defesa_inimigo == 'Nenhuma':
     print(f'voce causou {dano_temporario} de dano.')
     print(f'o inimigo estava usando defesa {tipo_defesa_inimigo}')
-    
 elif tipo_defesa_inimigo == 'Reflexiva':
     print(f'voce causou {dano_base} de dano.')
     print(f'O inimigo usou {tipo_defesa_inimigo} e você recebeu {dano_temporario} de volta.')
